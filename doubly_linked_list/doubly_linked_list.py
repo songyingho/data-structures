0a0cf69a8c222040eab6e309c6d88dcb52687971
Yen Szen <55312065+yenszen@users.noreply.github.com>
2020-07-27 23:46:08 +0800

    completed MVP

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -44,7 +44,9 @@ class DoublyLinkedList:
     Returns the value of the removed Node.
     """
     def remove_from_head(self):
-        pass
+        value = self.head.value
+        self.delete(self.head)
+        return value
             
     """
     Wraps the given value in a ListNode and inserts it 
@@ -69,21 +71,27 @@ class DoublyLinkedList:
     Returns the value of the removed Node.
     """
     def remove_from_tail(self):
-        pass
+        value = self.tail.value
+        self.delete(self.tail)
+        return value
             
     """
     Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List.
     """
     def move_to_front(self, node):
-        pass
+        node_val = node.value
+        self.delete(node)
+        self.add_to_head(node_val)
         
     """
     Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List.
     """
     def move_to_end(self, node):
-        pass
+        node_val = node.value
+        self.delete(node)
+        self.add_to_tail(node_val)
 
     """
     Deletes the input node from the List, preserving the 
